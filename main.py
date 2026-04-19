6adee8c09c8af676312f4bf66471cc0b6be72fc0
b6132766705-coder <b6132766705@gmail.com>
2026-04-18 22:04:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, Message
 # --- НАСТРОЙКИ ---
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = 1316137517  # ЗАМЕНИ НА СВОЙ ID
-DB_PATH = "/app/data/butya.db
+DB_PATH = "/app/data/butya.db"
 "
 
 # --- БАЗА ДАННЫХ ---
